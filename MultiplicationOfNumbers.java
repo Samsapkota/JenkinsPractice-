2cbf99ace1bf8cc39dbe2878a13d58c0a97f12ba
Shyam Sapkota <shyamsapkotaofficial@gmail.com>
2023-04-28 19:47:39 -0400

    My first commit of java file

---
+++ MultiplicationOfNumbers.java
@@ -0,0 +1,9 @@
+public class MultiplicationOfNumbers {
+    public static void main(String[] args) {
+        int result = 1;
+        for (int i = 1; i <= 10; i++) {
+            result *= i;
+        }
+        System.out.println("The multiplication of numbers from 1 to 10 is: " + result);
+    }
+}
