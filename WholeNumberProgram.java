10bab511f225cc01ffa7ab6ab3797deae32bd6db
Shyam Sapkota <shyamsapkotaofficial@gmail.com>
2023-04-30 14:11:35 -0400

    Hello this is my java file

---
+++ WholeNumberProgram.java
@@ -0,0 +1,26 @@
+public class WholeNumberProgram {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        
+        // Addition
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+        
+        // Subtraction
+        int diff = num1 - num2;
+        System.out.println("Difference: " + diff);
+        
+        // Multiplication
+        int product = num1 * num2;
+        System.out.println("Product: " + product);
+        
+        // Division
+        int quotient = num1 / num2;
+        System.out.println("Quotient: " + quotient);
+        
+        // Modulo (remainder)
+        int remainder = num1 % num2;
+        System.out.println("Remainder: " + remainder);
+    }
+}
