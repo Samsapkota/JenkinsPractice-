e0cb1456518ef257c7e34de7c9b4ee22f196dfc6
Samsapkota <123140415+Samsapkota@users.noreply.github.com>
2023-04-30 14:15:41 -0400

    Delete MultiplicationOfNumbers.java

--- MultiplicationOfNumbers.java
+++
@@ -1,9 +0,0 @@
-public class MultiplicationOfNumbers {
-    public static void main(String[] args) {
-        int result = 1;
-        for (int i = 1; i <= 10; i++) {
-            result *= i;
-        }
-        System.out.println("The multiplication of numbers from 1 to 10 is: " + result);
-    }
-}
